eba19ec714cb00e133941a19b18680b1e7bdb14b
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-08 20:53:11 +0530

    Updating Code

---
+++ src/utils/token.js
@@ -0,0 +1,26 @@
+export const tokenUtils = {
+  getToken: () => {
+    return localStorage.getItem('token');
+  },
+
+  setToken: (token) => {
+    localStorage.setItem('token', token);
+  },
+
+  removeToken: () => {
+    localStorage.removeItem('token');
+  },
+
+  getUser: () => {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  },
+
+  setUser: (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+  },
+
+  removeUser: () => {
+    localStorage.removeItem('user');
+  }
+};
